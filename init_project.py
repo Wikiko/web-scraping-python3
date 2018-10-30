000c61463127c1807c417f5f39745fb660745548
mobilef5 <mobilef5@Mac-mini-de-mobilef5.local>
2018-10-30 08:49:21 -0300

    Correção da automatização para começar o projeto.

--- init_project.py
+++ init_project.py
@@ -9,7 +9,7 @@ def install_virtualenv(pip_version = ''):
         print('Something happen wrong, ', e)
         return False
 
-def create_environment():
+def create_environment(env_name):
     print("Let's create an environment with name: {env}".format(env=env_name))
     try:
         result = os.popen('virtualenv {env_name}'.format(env_name=env_name)).read()
@@ -23,7 +23,6 @@ def install_env_dependences():
     try:
         if 'windows'.upper() in system_platform.upper():
             result = os.popen('cd ./{env_name}/Scripts && .\acivate.bat && pip install -r requirements.txt')
-        else:
     except Exception as e:
         print(e)
 
@@ -33,16 +32,16 @@ def main():
     python_version = os.popen('python --version').read()
     if '3' in python_version:
         instalation_result = install_virtualenv()
-    elif '2' in python_version:
-        another_python_version = os.popen('python3 --version')
+    else:
+        another_python_version = os.popen('python3 --version').read()
         if '3' in another_python_version:
             instalation_result = install_virtualenv('3')
     
     if instalation_result is True:
         print('virtualenv installed...')
-        env_creation_result = create_environment()
+        env_creation_result = create_environment(env_name)
         if env_creation_result is True:
-            print('nv created with sucessfull')
+            print('env created with sucessfull')
 
             print('Congratulations all is ready to go aread...')
     else:
